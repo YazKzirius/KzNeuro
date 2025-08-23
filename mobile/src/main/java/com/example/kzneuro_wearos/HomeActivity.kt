197cefc2f5703938aa704e62dfd6ca6afd496c00
yazeed-fair <yazeed@footballairesearch.com>
2025-08-23 20:48:20 +0100

    Additions: - Added Landing page to both versions of app Android and WearOS. - Added dashboard UI and bottom navigation to app - Set up testing for mobile and wear. - Added Basic UserAuth into app - 22/08/25 - Ready to start collecting bioinformatics data. Future Additions: - Add bioinformatics data collection through api's and map to cognitive health in front-end - 23/08/25 - Fix bugs and add any remaining touch ups, finalise documentation - 24/08/25

--- mobile/src/main/java/com/example/kzneuro_wearos/HomeActivity.kt
+++ mobile/src/main/java/com/example/kzneuro_wearos/HomeActivity.kt
@@ -5,8 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+    var fragment: Fragment = Dashboard()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +19,38 @@ class HomeActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //This function creates new fragments and adds basic functionality
+        createFragments()
     }
+    //This function creates new fragments based of the navigation bar
+    fun createFragments() {
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        //Setting Dashboard to default
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main, fragment)
+            .commit()
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            val id = item.itemId
+            fragment = when (id) {
+                R.id.nav_dashboard -> Dashboard()
+                R.id.nav_reports -> Reports()
+                R.id.nav_settings -> Settings()
+                else -> Dashboard()
+            }
+            if (fragment != null) {
+                openFragment(fragment)
+                return@setOnItemSelectedListener true
+            } else {
+                return@setOnItemSelectedListener false
+            }
+        }
+    }
+    //This function opens a fragment based of navigation bar
+    private fun openFragment(fragment: Fragment) {
+        // Example: Replace fragment or perform any action
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main, fragment)
+            .commit()
+    }
+
 }
